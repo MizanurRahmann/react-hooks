66f208315a61fe19ade027dfcf39ee76ebc68dfa
MizanurRahmann <mr926560@gmail.com>
2020-06-22 00:08:49 +0600

    useEffect with condition.

--- src/Components/UseEffectOne.js
+++ src/Components/UseEffectOne.js
@@ -2,13 +2,17 @@ import React, {useEffect, useState} from 'react'
 
 function UseEffectOne() {
     const [count, setCount] = useState(0);
+    const [name, setName] = useState('');
     
     useEffect(() => {
-        document.title = `You clicked ${count} times.`;
-    })
+            //2nd argument is a array that include the variable in which useEffect depends on.
+            console.log("Updating the js.")
+            document.title = `You clicked ${count} times.`;
+    }, [count])
 
     return (
         <div>
+            <input type="text" value={name} onChange={e => setName(e.target.value)}/>
             <button onClick={() => setCount(count + 1)}>Click {count} times</button>
         </div>
     )
