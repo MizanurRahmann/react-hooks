840cf57b8d2a68a65b7dbb681bd84f3674cbc1a9
MizanurRahmann <mr926560@gmail.com>
2020-06-25 20:19:51 +0600

    useContext.

--- src/Components/UserComponentE.js
+++ src/Components/UserComponentE.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import UserComponentF from './UserComponentF'
+import {UserContext, ChannelContext} from '../App';
+
 function UserComponentE() {
+    const user = useContext(UserContext);
+    const channel = useContext(ChannelContext);
+
     return (
         <div>
-            <UserComponentF />
+            {user} - {channel}
         </div>
     )
 }
